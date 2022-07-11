f37a7e7951fab0339269f99c91bf7a975590641c
gokul81 <105714204+gokul81@users.noreply.github.com>
2022-07-11 14:56:38 -0700

    form input

--- src/components/GetInputs.vue
+++ src/components/GetInputs.vue
@@ -1,5 +1,17 @@
 <template>
-  <v-content>Inputs</v-content>
+  <v-content>
+    <v-card-title>
+      <h2>Add New Task</h2>
+    </v-card-title>
+<v-form class="px-3">
+  <v-text-field label="ID NO"></v-text-field>
+   <v-text-field label="Name"></v-text-field>
+   <v-textarea label="Description" ></v-textarea>
+   <!-- <v-date-picker class="" ></v-date-picker> -->
+   <v-btn color="thin" dark  >Submit</v-btn>
+</v-form>
+  </v-content>
+
 </template>
 
 <script>
