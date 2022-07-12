30d18328bec416703310a25c398e61743fe58c78
Vimal KR <vimalkr141@gmail.com>
2022-07-12 11:51:08 +0530

    nav and routes

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -2,7 +2,7 @@
   <v-footer height="auto" color="indigo" dark>
     <v-layout justify-center row wrap>
       <v-flex color="indigo" dark py-3 text-xs-center white--text xs12>
-        &copy;2018 — <strong>Vuetify</strong>
+        &copy;2022 — <strong>Team Collab</strong>
       </v-flex>
     </v-layout>
   </v-footer>
