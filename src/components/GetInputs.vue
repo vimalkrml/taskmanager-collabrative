abb790441e095fb67ac33330f65106385fa89bb4
Vimal KR <vimalkr141@gmail.com>
2022-07-12 15:06:57 +0530

    form

--- src/components/GetInputs.vue
+++ src/components/GetInputs.vue
@@ -34,6 +34,31 @@
             ></v-select>
           </v-col>
         </v-row>
+        <v-col cols="12" sm="6" md="4">
+          <v-menu
+            v-model="menu2"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="date"
+                label="Picker without buttons"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="date"
+              @input="menu2 = false"
+            ></v-date-picker>
+          </v-menu>
+        </v-col>
       </v-container>
     </v-form>
   </v-content>
@@ -53,6 +78,12 @@ export default {
       (v) => v.length <= 100 || "Description must be less than 100 characters",
     ],
     items: ["True", "False"],
+    date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+      .toISOString()
+      .substr(0, 10),
+    menu: false,
+    modal: false,
+    menu2: false,
   }),
 };
 </script>
