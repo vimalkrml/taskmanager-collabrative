c89f2b238ac904206ab7a558548453e9e20edab4
M.Karthiheyan <davidkarthick85@gmail.com>
2022-07-13 12:17:25 +0530

    main page

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,49 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
-        tasks: [{ id: '1', name: 'vimal', description: 'tasks done', status: 0, duedate: new Date() }]
+  state: {
+    tasks: null,
+  },
+  mutations: {
+    loadTasks: (state, tasks) => {
+      console.log(tasks);
+      state.tasks = tasks;
+      console.log(state.tasks);
     },
-    mutations: {},
-    actions: {},
-    modules: {},
-    getters: {
-        getNameAndDescription: (state) => {
-            return state.tasks[0].name + ' ' + state.tasks[0].description
+    onComplete(state, payload) {
+      //   console.log(state.tasks);
+      state.tasks.forEach((task) => {
+        if (task.id === payload.id) {
+          task.completed = payload.completed;
+          //   console.log(task);
         }
-    }
-})
+      });
+      //   console.log(state.tasks);
+      const updateRequest = {
+        method: "PATCH",
+        body: JSON.stringify({
+          completed: payload.completed,
+          status: payload.completed ? "Done" : "Not Done",
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      };
+      fetch("http://localhost:3000/tasks/" + payload.id, updateRequest);
+    },
+  },
+  actions: {
+    getTasks: async (context) => {
+      const res = await fetch("http://localhost:3000/tasks");
+      const data = await res.json();
+      console.log(data);
+
+      context.commit("loadTasks", data);
+    },
+  },
+  modules: {},
+  getters: {},
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,24 @@
 <template>
   <v-content>
-    {{ posts }}
+    <v-card v-for="task in tasks" :key="task.id" class="mb-2">
+      <v-row align="center">
+        <v-col cols="1">
+          <v-checkbox
+            v-model="task.completed"
+            @click="onComplete(task.id, task.completed)"
+            class="ml-2"
+          ></v-checkbox>
+        </v-col>
+        <v-col cols="9">
+          <h1 class="text-left text-2xl">
+            {{ task.title }}
+          </h1>
+        </v-col>
+        <v-col cols="2">
+          <v-card-text>{{ task.status }}</v-card-text>
+        </v-col>
+      </v-row>
+    </v-card>
   </v-content>
 </template>
 
@@ -8,11 +26,28 @@
 export default {
   data() {
     return {
-      posts: [],
+      tasks: [],
     };
   },
+  methods: {
+    onComplete(id, completed) {
+      // console.log("dddd");
+      this.$store.commit("onComplete", { id, completed });
+      this.tasks.forEach((task) => {
+        if (task.id === id) {
+          task.completed = completed;
+          task.status = completed ? "Done" : "Not Done";
+          //   console.log(task);
+        }
+      });
+    },
+  },
   mounted() {
-    this.posts = this.$store.getters.getNameAndDescription;
+    this.$store.dispatch("getTasks").then(() => {
+      this.tasks = this.$store.state.tasks;
+      console.log(this.tasks);
+    });
+    // console.log(JSON.parse(JSON.stringify(this.tasks)));
   },
 };
 </script>
