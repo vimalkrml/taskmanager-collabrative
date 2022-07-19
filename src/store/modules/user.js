6e375c716122462d7e1677e1aaffab87b6288b53
M.Karthiheyan <davidkarthick85@gmail.com>
2022-07-19 18:34:33 +0530

    minor changes

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,67 +1,67 @@
 export default {
-    namespaced: true,
-    state: {
-        users: [],
-        user: {},
+  namespaced: true,
+  state: {
+    users: [],
+    user: {},
+  },
+  mutations: {
+    USER_INDEX(state, users) {
+      // console.log(users);
+      state.users = users;
+      // console.log(state.users);
     },
-    mutations: {
-        USER_INDEX(state, users) {
-            // console.log(users);
-            state.users = users;
-            // console.log(state.users);
-        },
-        USER_DELETE(state, payload) {
-            // console.log("hii");
-            const mutateData = state.users.filter((user) => user.id !== payload);
-            console.log(mutateData);
-            state.users = mutateData;
-            // console.log(state.users);
-        },
-        USER_ADD(state, payload) {
-            console.log(state.users);
-            // JSON.parse(JSON.stringify(state.users)).push(payload);
-            state.users.push(payload);
-            // console.log(payload)
-        },
-        USER_SHOW(state, payload) {
-            state.user = payload;
-        }
+    USER_DELETE(state, payload) {
+      // console.log("hii");
+      const mutateData = state.users.filter((user) => user.id !== payload);
+      console.log(mutateData);
+      state.users = mutateData;
+      // console.log(state.users);
     },
+    USER_ADD(state, payload) {
+      console.log(state.users);
+      // JSON.parse(JSON.stringify(state.users)).push(payload);
+      state.users.push(payload);
+      // console.log(payload)
+    },
+    USER_SHOW(state, payload) {
+      state.user = payload;
+    },
+  },
 
-    actions: {
-        user_index: async (context) => {
-            const res = await fetch("http://localhost:3000/users");
-            const data = await res.json();
-            console.log(data);
+  actions: {
+    user_index: async (context) => {
+      const res = await fetch("http://localhost:3000/users");
+      const data = await res.json();
+      console.log(data);
 
-            context.commit("USERS_INDEX", data);
-        },
-        user_delete: async (context, payload) => {
-            context.commit("USER_DELETE", payload);
-            await fetch("http://localhost:3000/users/" + payload, {
-                method: "DELETE",
-            });
-        },
-        user_add: (context, payload) => {
-            context.commit("USER_ADD", payload);
-            const newuser = { ...payload };
-            // console.log(newuser);
-            const requestOptions = {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(newuser),
-            };
-            fetch("http://localhost:3000/users", requestOptions);
-        },
-        user_show: async (context, payload) => {
-            const response = await fetch("http://localhost:3000/users/" + payload);
-            const json = await response.json();
-            console.log(json);
-            context.commit("USER_SHOW", json)
-        }
+      context.commit("USER_INDEX", data);
     },
-    getters: {
-        getUsers: (state) => state.users,
-        getUser: (state) => state.user,
+    user_delete: async (context, payload) => {
+      context.commit("USER_DELETE", payload);
+      await fetch("http://localhost:3000/users/" + payload, {
+        method: "DELETE",
+      });
     },
+    user_add: (context, payload) => {
+      context.commit("USER_ADD", payload);
+      const newuser = { ...payload };
+      // console.log(newuser);
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newuser),
+      };
+      fetch("http://localhost:3000/users", requestOptions);
+    },
+    user_show: async (context, payload) => {
+      const response = await fetch("http://localhost:3000/users/" + payload);
+      const json = await response.json();
+      console.log(json);
+      context.commit("USER_SHOW", json);
+    },
+  },
+  getters: {
+    getUsers: (state) => state.users,
+    getUser: (state) => state.user,
+  },
 };
--- src/views/UsersPage.vue
+++ src/views/UsersPage.vue
@@ -1,8 +1,26 @@
 <template>
-  <v-main>Users page</v-main>
+  <v-main>
+    <v-card v-for="user in users" :key="user.id" class="mb-2" hover>
+      <v-row align="center">
+        <v-col cols="2">
+          {{ user.name }}
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-main>
 </template>
 
 <script>
-export default {};
+import { mapActions, mapState } from "vuex";
+export default {
+  methods: {
+    ...mapActions("user", ["user_index"]),
+  },
+  computed: {
+    ...mapState("user", ["users"]),
+  },
+  mounted() {
+    this.user_index();
+  },
+};
 </script>
-
