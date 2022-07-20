4f34be208cf5c11de1c2500eded9cd22d3af417b
Vimal KR <vimalkr141@gmail.com>
2022-07-20 16:30:10 +0530

    loader

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,63 +1,8 @@
 <template>
-  <!-- <v-main>
-    <v-toolbar-title
-      ><v-btn depressed to="/create">Create Task</v-btn></v-toolbar-title
-    >
-    <v-btn class="mt-20" plain loading v-if="isLoading"></v-btn>
-    <v-content v-else class="mx-0 md:mx-10">
-      <v-card v-for="task in tasks" :key="task.id" class="mb-2" hover>
-        <v-row align="center">
-          <v-col cols="2">
-            <v-checkbox
-              v-model="task.completed"
-              @click="onComplete(task.id, task.completed)"
-              class="ml-5"
-            ></v-checkbox>
-          </v-col>
-          <v-snackbar v-model="snackbar" :multi-line="multiLine">
-            {{ text }}
-            <template v-slot:action="{ attrs }">
-              <v-btn color="red" text v-bind="attrs" @click="snackbar = false">
-                Close
-              </v-btn>
-            </template>
-          </v-snackbar>
-          <v-col cols="4">
-            <v-card-title
-              :class="{ 'line-through text-gray-400': task.completed }"
-              v-html="task.name"
-            >
-               {{ task.title }}
-   </v-card-title>
-          </v-col>
-          <v-spacer />
-
-          <v-col col="2">{{ get_username(task.user_id) }}</v-col>
-          <v-col cols="2">
-            <v-btn
-              :class="{ 'line-through text-gray-400': task.completed }"
-              icon
-              class="font-bold"
-              >{{ task.date }}</v-btn
-            >
-          </v-col>
-          <v-spacer />
-          <v-col cols="2">
-            <lord-icon
-              @click="task_delete(task.id)"
-              src="https://cdn.lordicon.com/qsloqzpf.json"
-              trigger="hover"
-              colors="primary:#121331"
-              class="cursor-pointer w-7"
-            >
-            </lord-icon>
-          </v-col>
-        </v-row>
-      </v-card>
-    </v-content>
-  </v-main> -->
   <v-main>
-    <v-simple-table>
+    <v-btn class="mb-5" depressed to="/create">Create Task</v-btn>
+    <v-btn class="mb-5" plain loading v-if="isLoading"></v-btn>
+    <v-simple-table v-else>
       <template v-slot:default>
         <thead>
           <tr>
